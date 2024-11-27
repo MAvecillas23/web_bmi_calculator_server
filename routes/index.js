d5dc650e9a46e4539e6afc8d50cc53f72cdff70d
Michael <email@>
2024-11-27 13:47:52 -0600

    added code that displays an error template if user enters 0 or nothing in their height and weight fields.

--- routes/index.js
+++ routes/index.js
@@ -13,13 +13,23 @@ router.get('/calculate', function(req, res,next) {
     // get the query data from the input fields
     const formData = req.query
     console.log(formData)
+    // check is user entered height and weight
+    if (formData.height === '' || formData.weight === '') {
+        res.render('error_message', {error: 'Height and Weight fields must be entered'})
+    }
+    // check if user entered 0 in the height and weight fields
+    else if (formData.height == 0 || formData.weight == 0) {
+        res.render('error_message', {error: 'The Height and Weight fields cannot be 0'})
+    }
+    else {
+        // calculate bmi using the query data
+        const bmiDec = formData.weight / (formData.height * formData.height)
+        // round to 2 decimals
+        const bmi = bmiDec.toFixed(2)
+        // render the results template with bmi calculation
+        res.render('results', {bmi: bmi})
+    }
 
-    // calculate bmi using the query data
-    const bmiDec = formData.weight / (formData.height * formData.height)
-    // round to 2 decimals
-    const bmi = bmiDec.toFixed(2)
-    // render the results template with bmi calculation
-    res.render('results', {bmi: bmi})
 })
 
 
